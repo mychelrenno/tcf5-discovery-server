96e19e3ed815957f6547ffa3ebd5155b857a6229
Mychel Renno <mychelrenno@gmail.com>
2026-03-28 15:22:31 -0300

    feat - feito discovery server

--- src/main/java/com/devrenno/tcf5/discovery/server/Application.java
+++ src/main/java/com/devrenno/tcf5/discovery/server/Application.java
@@ -2,12 +2,14 @@ package com.devrenno.tcf5.discovery.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class Tcf5DiscoveryServerApplication {
+@EnableEurekaServer
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Tcf5DiscoveryServerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
