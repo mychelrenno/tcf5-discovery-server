fa533c25fbc149d3102bad8c8131364ad928a17b
Mychel Renno <mychelrenno@gmail.com>
2026-03-28 12:36:11 -0300

    criado projeto inicial

---
+++ src/main/java/com/devrenno/tcf5/discovery/server/Tcf5DiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.devrenno.tcf5.discovery.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tcf5DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tcf5DiscoveryServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devrenno/tcf5/discovery/server/Tcf5DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devrenno.tcf5.discovery.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tcf5DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
